99a02d9cc44c10a8fa9a9a31ea0a95cf7f1b2ba3
edo:3 <eduardofbaltazar@gmail.com>
2024-12-22 04:41:19 +0000

    updated followed

--- resources/views/pages/user/followed.blade.php
+++ resources/views/pages/user/followed.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Followed')
 
 @section('content')
-    <div class="rectangle-div">
+    <div class="p-20">
         <h1>{{ $user->username }}'s Followed Auctions</h1>
 
         @if($followedAuctions->count() === 0)
@@ -12,10 +12,10 @@
             <div class="grid grid-cols-3 gap-4">
                 @foreach ($followedAuctions as $auction)
                     <a href="{{ route('auction.show', $auction) }}">
-                        <div class="bg-gray-100 p-4 rounded shadow">
-                            <div class="w-full h-48 bg-gray-300 rounded" style="background-image: url('{{ asset('storage/' . $auction->picture) }}'); background-size: cover;"></div>
-                            <h3 class="mt-2 text-lg font-semibold">{{ $auction->title }}</h3>
-                            <p class="text-sm text-gray-500">Current Bid: ${{ $auction->current_bid }}</p>
+                        <div class="bg-gray-100 p-4 rounded shadow user-auction-lists">
+                            <div class="w-full h-64 bg-gray-300 rounded" style="background-image: url('{{ asset('storage/' . $auction->picture) }}'); background-size: cover;"></div>
+                            <h3 class="mt-2 text-3xl font-semibold">{{ $auction->title }}</h3>
+                            <p class="text-xl text-white">Current Bid: ${{ $auction->current_bid }}</p>
                         </div>
                     </a>
                 @endforeach
